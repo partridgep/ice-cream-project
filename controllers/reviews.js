0314ce5362ee363f0e042d83ce80569589b81145
partridgep <paul.partridg@gmail.com>
2020-06-13 12:45:16 -0400

    updates reviewer when rating ice cream

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -1,4 +1,5 @@
 const IceCream = require('../models/icecream');
+const User = require('../models/user');
 
 module.exports = {
  addRating
@@ -9,9 +10,16 @@ function addRating(req, res) {
     IceCream.findById(req.params.id, function(err, iceCream) {
         console.log(req.body);
         console.log(req.user)
-        // we push the review into the movie's review array
+        //  push the review into the movie's review array
         iceCream.reviews.push(req.body);
-        // save the movie
+        // find user that's rating the ice cream
+        User.findById(req.user, function(err, reviewer) {
+            console.log(reviewer);
+            // push ice cream into user's array of rated ice creams and save
+            reviewer.ratedIceCreams.push(req.params.id);
+            reviewer.save(function(err) {console.log(err)});
+        });
+        // save the ice cream
         iceCream.save(function(err) {
             // redirect the user
             res.redirect(`/flavors/${iceCream.flavorName}`);
