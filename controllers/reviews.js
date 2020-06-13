2d6723656ea2dda754ae0436c82d6a8591406da9
partridgep <paul.partridg@gmail.com>
2020-06-13 12:11:28 -0400

    shows user rating once created

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -8,6 +8,7 @@ function addRating(req, res) {
     // Find the ice cream
     IceCream.findById(req.params.id, function(err, iceCream) {
         console.log(req.body);
+        console.log(req.user)
         // we push the review into the movie's review array
         iceCream.reviews.push(req.body);
         // save the movie
